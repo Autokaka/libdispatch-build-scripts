818cd6536a0d1b62aba4b776e0faf35c4722073f
Autokaka <qq1909698494@gmail.com>
2023-01-17 04:01:48 +0800

    feat: android build scripts

---
+++ build_android.sh
@@ -0,0 +1,11 @@
+rm -rf build-android
+cmake . -B build-android -G Ninja \
+        -DCMAKE_C_COMPILER=clang \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_SYSTEM_NAME=Android \
+        -DCMAKE_SYSTEM_VERSION=21 \
+        -DCMAKE_ANDROID_NDK=$ANDROID_NDK \
+        -DCMAKE_SYSTEM_PROCESSOR="armv7-a" \
+        -DBUILD_SHARED_LIBS=OFF
+cd build-android
+ninja
